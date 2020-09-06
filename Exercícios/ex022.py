1680c2f84fadba434c604c38fbc3fc7a9a1cb977
Hugo Corrêa <hugodemorais@live.com>
2020-09-06 00:17:15 -0300

    Exercícios

---
+++ Exercícios/ex022.py
@@ -0,0 +1,4 @@
+#Crie um programa que leia o nome completo de uma pessoa e mostre:
+#– O nome com todas as letras maiúsculas e minúsculas.
+#– Quantas letras aotodo (sem considerar espaços).
+#– Quantas letras tem o primeiro nome.
